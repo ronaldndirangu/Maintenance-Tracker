8c376373978c8ad2b33c802c85936981cf62634a
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2018-05-30 15:12:05 +0300

    Improved dir structure

--- API/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-def create_app(config_filename=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile(config_filename)
-    return app
