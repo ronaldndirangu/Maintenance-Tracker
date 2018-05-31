b4a7ce98d0b1d80102cff6ee38f98c4b2f8c45e7
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2018-05-31 22:52:51 +0300

    Added changes to models

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,8 @@
 from flask import Flask, Response, abort, request, jsonify
 import json
 from instance.config import app_config
+from app.models import users, requests
 
-
-users = [{"user_id": '1', "firstname": "Ron", "lastname": "Ndi", 
-		"email": "ron.ndi@gmail.com","password": 'rrrrnnnn'	},
-		{"user_id": '2', "firstname": "Trev","lastname": "Kar", 
-		"email": "trev.kar@gmail.com","password": 'ttttkkkk'},
-		]
-
-requests = [{"request_id":1,"date": '12/1/2018',"title": "Replace Motor",
-			"location": "Liquid Plant",	"priority":"High",
-			"description": "Motor overheating due to unbalanced windings",
-			"status":"pending",
-			"created_by":"John"},
-			{"request_id":2,"date": '12/1/2018',"title": "Replace Sensor",
-			"location": "Soap Plant","priority":"Medium",
-			"description": "Sensor misbehaving needs replacement",
-			"status":"approved",
-			"created_by":"Mary"},
-			]
 
 # define create_app to create and return Flask app
 def create_app(config_name):
@@ -106,5 +89,5 @@ def create_app(config_name):
 			}
 			users.append(new_user)
 			return jsonify(new_user), 201
-    
+
 	return app
--- app/models.py
+++ app/models.py
@@ -23,3 +23,21 @@ class Request:
 
     def __str__(self):
         return '<User: {}+" "+{}>'.format(self.title, self.description)
+
+users = [{"user_id": '1', "firstname": "Ron", "lastname": "Ndi", 
+		"email": "ron.ndi@gmail.com","password": 'rrrrnnnn'	},
+		{"user_id": '2', "firstname": "Trev","lastname": "Kar", 
+		"email": "trev.kar@gmail.com","password": 'ttttkkkk'},
+		]
+
+requests = [{"request_id":1,"date": '12/1/2018',"title": "Replace Motor",
+			"location": "Liquid Plant",	"priority":"High",
+			"description": "Motor overheating due to unbalanced windings",
+			"status":"pending",
+			"created_by":"John"},
+			{"request_id":2,"date": '12/1/2018',"title": "Replace Sensor",
+			"location": "Soap Plant","priority":"Medium",
+			"description": "Sensor misbehaving needs replacement",
+			"status":"approved",
+			"created_by":"Mary"},
+			]
