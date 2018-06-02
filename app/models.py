0c5d966d467d9905fae05ecb9edaa77552a544f8
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2018-06-02 18:22:48 +0300

    Added more tests

--- app/models.py
+++ app/models.py
@@ -12,7 +12,8 @@ class User:
 
 class Request:
 
-    def __init__(self, date, title, location, priority, description, status, created_by):
+    def __init__(self, request_id, date, title, location, priority, description, status, created_by):
+        self.request_id = request_id
         self.date = date
         self.title = title
         self.location = location
@@ -23,21 +24,3 @@ class Request:
 
     def __str__(self):
         return '<User: {}+" "+{}>'.format(self.title, self.description)
-
-users = [{"user_id": '1', "firstname": "Ron", "lastname": "Ndi", 
-		"email": "ron.ndi@gmail.com","password": 'rrrrnnnn'	},
-		{"user_id": '2', "firstname": "Trev","lastname": "Kar", 
-		"email": "trev.kar@gmail.com","password": 'ttttkkkk'},
-		]
-
-requests = [{"request_id":1,"date": '12/1/2018',"title": "Replace Motor",
-			"location": "Liquid Plant",	"priority":"High",
-			"description": "Motor overheating due to unbalanced windings",
-			"status":"pending",
-			"created_by":"John"},
-			{"request_id":2,"date": '12/1/2018',"title": "Replace Sensor",
-			"location": "Soap Plant","priority":"Medium",
-			"description": "Sensor misbehaving needs replacement",
-			"status":"approved",
-			"created_by":"Mary"},
-			]
