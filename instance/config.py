b3aa401da3f28ba1949ce86b7b7c79e141218b59
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2018-06-11 09:54:07 +0300

    Added production DATABASE_URL

--- instance/config.py
+++ instance/config.py
@@ -1,8 +1,9 @@
-#config file
+# config file
 
 import os
 
 SECRET_KEY = "Mysecret"
+
 
 class Config(object):
     """Base config class"""
@@ -14,24 +15,29 @@ class DevConfig(Config):
     """Configuration for development"""
     DEBUG = True
 
+
 class TestConfig(Config):
     """Test config"""
     TESTING = True
     DEBUG = True
     CSRF_ENABLED = False
 
+
 class StagingConfig(Config):
     """Staging configuration"""
     DEBUG = True
+
 
 class ProductionConfig(Config):
     """Staging for production"""
     DEBUG = False
     TESTING = False
+    DATABASE_URL: postgres: // cmcfpalbxxkgpo: 92a26a779471689f49b39abefd444ca966e1e5ad6ae0bca99ffaf635c24cbea1@ec2 - 23 - 23 - 130 - 158.compute - 1.amazonaws.com: 5432 / deqp08uur6q2gh
+
 
 app_config = {
-            'development':DevConfig,
-            'testing':TestConfig,
-            'staging':StagingConfig,
-            'production':ProductionConfig,
-            }            
+    'development': DevConfig,
+    'testing': TestConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
